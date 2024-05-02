c61a5efff8cb08f9112ceb4ec1a240d774436f04
Jakub Westfalewski <j.westfalewski@gmail.com>
2024-05-02 23:11:31 +0200

    electron app config

--- google-chrome/install.bash
+++ google-chrome/install.bash
@@ -6,3 +6,6 @@ fi
 
 rm /home/jwest/.config/chrome-flags.conf
 ln -sf /home/jwest/.dotfiles/google-chrome/chrome-flags.conf /home/jwest/.config/chrome-flags.conf
+
+rm /home/jwest/.config/electron-flags.conf
+ln -sf /home/jwest/.dotfiles/google-chrome/electron-flags.conf /home/jwest/.config/electron-flags.conf
