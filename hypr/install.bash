494401320e846d92bddf37ac5db8ae3345b46aaf
Jakub Westfalewski <j.westfalewski@gmail.com>
2024-05-05 18:07:09 +0200

    hypr wallpapaer

--- hypr/install.bash
+++ hypr/install.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm hyprland
+sudo pacman -S --noconfirm hyprland hyprpaper
 rm -rf /home/jwest/.config/hypr
 ln -sf /home/jwest/.dotfiles/hypr /home/jwest/.config/hypr
